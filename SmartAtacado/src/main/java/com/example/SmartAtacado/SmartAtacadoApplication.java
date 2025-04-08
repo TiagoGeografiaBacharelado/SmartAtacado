79fff2803998cbdbb41bf521bd997403f7e4bf3c
TiagoCarneiroDoNascimento <tiago.tcan@gmail.com>
2025-04-07 22:33:43 -0300

    Update
    -Estrutura organizacional do sistema

---
+++ SmartAtacado/src/main/java/com/example/SmartAtacado/SmartAtacadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartAtacado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAtacadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAtacadoApplication.class, args);
+	}
+
+}
---
+++ SmartAtacado/src/test/java/com/example/SmartAtacado/SmartAtacadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartAtacado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAtacadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
